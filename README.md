d1999041d2ff955cae87b63c2b54d461d63331f5
Juho Vähä-Herttua <juho.vaha-herttua@futurice.com>
2014-03-22 12:49:38 +0200

    Add a simple mock server

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 festapp-server
 ==============
+
+Run the project:
+
+`node server`
+
+Supported URLs:
+
+```
+Dynamic data:
+http://localhost:8080/api/artistit
+http://localhost:8080/api/uutiset
+http://localhost:8080/api/info
+http://localhost:8080/api/ohjelma
+
+Static data:
+http://localhost:8080/api/general
+http://localhost:8080/api/services
+http://localhost:8080/api/arrival
+```
+
---
+++ server.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+var supported = ['artistit', 'uutiset', 'info', 'ohjelma', 'general', 'services', 'arrival'];
+
+var server = http.createServer(function(req, res) {
+  var pathname = url.parse(req.url).pathname;
+  var regex = new RegExp("^\\/api\\/("+supported.join("|")+")$");
+  var matched = pathname.match(regex);
+  if (matched) {
+    fs.readFile('data/'+matched[1]+'.json', function(err, data) {
+      if (err) {
+        res.writeHead(500, 'Internal server error');
+        res.end('Server Error');
+      } else {
+        res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404, 'Requested resource not found');
+    res.end('Not Found');
+  }
+});
+server.listen(8080);
