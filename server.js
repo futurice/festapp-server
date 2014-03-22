4657bc85ed8b16acd29d9e27a83bb96036fa429b
Juho Vähä-Herttua <juho.vaha-herttua@futurice.com>
2014-03-22 19:39:03 +0200

    Clean up server.js

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 var express = require('express');
-
 var http = require('http');
 var url = require('url');
-var fs = require('fs');
 
-var supported = ['faq', 'artists', 'news', 'program', 'general', 'services'];
 var middleware = function(req, res) {
   var pathname = url.parse(req.url).pathname;
-  var regex = new RegExp("^\\/("+supported.join("|")+")$");
-  var matched = pathname.match(regex);
-  if (matched) {
-    res.type('application/json; charset=utf-8').sendfile('data/'+matched[1]+'.json');
-  } else {
-    res.writeHead(404, 'Requested resource not found');
-    res.end('Not Found');
-  }
+  res.type('application/json; charset=utf-8').sendfile('data'+pathname+'.json');
 };
 
-var app = express().use('/api', middleware);
+var app = express()
+  .use('/api', middleware);
+  .use('/public', express.static(__dirname + '/public'));
+
 var port = Number(process.env.PORT || 8080);
-app.use('/public', express.static(__dirname + '/public'));
 http.createServer(app).listen(port);
+console.log('Running at port '+port);
