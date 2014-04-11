697334a5f12cd4e6a98ed172d08c454b885a5f59
Jan Wikholm <jan.wikholm@relex.fi>
2014-04-11 20:26:50 +0300

    Twitter API integration squash
    /api/twitter/search/YOUR_SEARCH_HERE/num_tweets
    
    /api/twitter/username/USERNAME/num_tweets
    
    /api/twitter/hashtag/HASHTAG/num_tweets

--- server.js
+++ server.js
@@ -18,7 +18,18 @@ db.once('open', function callback () {
   console.log('Yay');
 });
 
+var twitter = require('./lib/twitter');
+var twatter = new twitter.twitter(process.env.TWITTER_API_KEY, process.env.TWITTER_SECRET);
+twatter.authenticate(function(success) {
+  if (!success) {
+    console.error("Authentication failed");
+  }
+})
 var app = express();
+
+app.use('/api/twitter/search/:search/:count?',  twitter.twitter.createHandler(twatter, 'search'))
+  .use('/api/twitter/user/:userSearch/:count?', twitter.twitter.createHandler(twatter, 'userSearch'))
+  .use('/api/twitter/hashtag/:hashtag/:count?', twitter.twitter.createHandler(twatter, 'hashtag'))
 app.use('/public', express.static(__dirname + '/public'));
 
 restify.serve(app, Artist);
@@ -26,6 +37,7 @@ restify.serve(app, Faq);
 restify.serve(app, News);
 restify.serve(app, Program);
 restify.serve(app, Stage);
+
 
 
 var port = Number(process.env.PORT || 8080);
