66fd6b2b3226bfcb20a69ac5cfd625567f881cae
Juuso Hyvönen <juuso.hyvonen@cs.helsinki.fi>
2014-04-11 21:14:39 +0300

    fixed paths

--- server.js
+++ server.js
@@ -7,8 +7,24 @@ var middleware = function(req, res) {
   res.type('application/json; charset=utf-8').sendfile('data'+pathname+'.json');
 };
 
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+    res.render('index');
+};
+
+exports.partials = function (req, res) {
+    var name = req.params.name;
+    res.render('partials/' + name);
+};
+
+
 var app = express()
   .use('/api', middleware)
+  .use('/admin', express.static(__dirname + '/admin'))
   .use('/public', express.static(__dirname + '/public'));
 
 var port = Number(process.env.PORT || 8080);
