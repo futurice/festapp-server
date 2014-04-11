6d1647e7ea508f74bbf7e0604d4483fcced31c87
Mikko Tamminen <mikko.k.tamminen@gmail.com>
2014-04-12 00:41:45 +0300

    first controller

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,16 @@
 'use strict';
 
 
-// Declare app level module which depends on filters, and services
+// Declare admin level module which depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+    'ngRoute',
+    'myApp.filters',
+    'myApp.services',
+    'myApp.directives',
+    'myApp.controllers'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'ArtistsListCtrl'});
+        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'ArtistsListCtrl'});
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }]);
