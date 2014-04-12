a006904cd2877f20edc6f1e7940db567361babb7
Ville Pulkkinen <ville.p.pulkkinen@gmail.com>
2014-04-12 18:43:05 +0300

    Basic controllers for event, news, locations and info

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -68,9 +68,16 @@ myAppControllers.
     }]);
 
 myAppControllers.
-    controller('HomeCtrl', ['$scope', function($scope) {
-        $scope.hello = 'Hola!';
-    }]);
+  controller('EventCtrl', ['$scope', '$routeParams', '$location', 'Event', function ($scope, $routeParams, $location, Event) {
+
+    Event.get({eventId: $routeParams.eventId}, function(event) {
+      $scope.event = event;
+    });
+    
+    $scope.saveEvent = function(){
+      Event.update({ eventId:$scope.event._id},$scope.event);
+    }
+  }]);
 
 myAppControllers.
     controller('NewsListCtrl', ['$scope', 'News', function($scope, News) {
@@ -78,9 +85,33 @@ myAppControllers.
     }]);
 
 myAppControllers.
+  controller('NewsCtrl', ['$scope', '$routeParams', '$location', 'News', function ($scope, $routeParams, $location, News) {
+
+    News.get({newsId: $routeParams.newsId}, function(news) {
+      $scope.news = news;
+    });
+
+    $scope.saveNews = function(){
+      News.update({ newsId:$scope.news._id},$scope.news);
+    }
+  }]);
+
+myAppControllers.
     controller('LocationsListCtrl', ['$scope', 'Locations',function($scope, Locations) {
         $scope.locations = query();
     }]);
+
+myAppControllers.
+  controller('LocationsCtrl', ['$scope', '$routeParams', '$location', 'Locations', function ($scope, $routeParams, $location, Locations) {
+
+    Locations.get({locationsId: $routeParams.locationsId}, function(locations) {
+      $scope.locations = locations;
+    });
+
+    $scope.saveLocations = function(){
+      Locations.update({ locationsId:$scope.locations._id},$scope.locations);
+    }
+  }]);
 
 myAppControllers.
     controller('InfoListCtrl', ['$scope', 'Info', function($scope, Info) {
@@ -88,6 +119,32 @@ myAppControllers.
     }]);
 
 myAppControllers.
-    controller('FestivalCtrl', ['$scope', 'Festival', function($scope, Festival) {
-        $scope.hello2 = Festival.query();
-    }]);
+  controller('InfoCtrl', ['$scope', '$routeParams', '$location', 'Info', function ($scope, $routeParams, $location, Info) {
+
+    Info.get({infoId: $routeParams.infoId}, function(info) {
+      $scope.info = info;
+    });
+
+    $scope.saveInfo = function(){
+      Info.update({ infoId:$scope.info._id},$scope.info);
+    }
+  }]);
+
+myAppControllers.
+  controller('FestivalCtrl', ['$scope', '$routeParams', '$location', 'Festival', function ($scope, $routeParams, $location, Festival) {
+
+    $scope.hello2 = Festival.query();
+
+    Festival.get({festivalId: $routeParams.festivalId}, function(festival) {
+      $scope.festival = festival;
+    });
+
+    $scope.saveInfo = function(){
+      Festival.update({ festivalId:$scope.festival._id},$scope.festival);
+    }
+  }]);
+
+myAppControllers.
+  controller('HomeCtrl', ['$scope', function($scope) {
+    $scope.hello = 'Hola!';
+  }]);
