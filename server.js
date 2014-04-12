ad84ef2449c51b7d20221a3c1bdb8464a683182d
Joni Salmi <josalmi@cs.helsinki.fi>
2014-04-12 08:33:11 +0300

    Instagram clean and nice

--- server.js
+++ server.js
@@ -7,37 +7,10 @@ var middleware = function(req, res) {
   res.type('application/json; charset=utf-8').sendfile('data'+pathname+'.json');
 };
 
-var instagram = function(req, res) {
-  var ig = require('instagram-node').instagram();
-  ig.use({ client_id: '',
-           client_secret: ''});
-  ig.tag_media_recent('ruisrock', function(err, medias, pagination, limit) {
-    res.send(JSON.stringify(parseIgMedia(medias)));
-  });
-}
-
-var parseIgMedia = function(medias){
-  var data = [];
-  medias.forEach(function(media){
-    data.push(parseIgMediaObject(media));
-  });
-  return data;
-}
-
-var parseIgMediaObject = function(media){
-  return {
-    link: media.link,
-    title: (media.caption==null)?"":media.caption.text,
-    thumbnail: media.images.thumbnail.url,
-    small_image: media.images.low_resolution.url,
-    image: media.images.standard_resolution.url,
-    likes: media.likes.count,
-    tags: media.tags
-  }
-}
+var instagram = require('./lib/instagram');
 
 var app = express()
-  .use('/api/instagram', instagram)
+  .use('/api/instagram', instagram.tagMedia)
   .use('/api', middleware)
   .use('/public', express.static(__dirname + '/public'));
 
