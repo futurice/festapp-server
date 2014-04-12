ffd738251efaabece4ae202c112255bc65635ba8
Timo Sand <timo.j.sand@gmail.com>
2014-04-12 23:40:51 +0300

    Added failing test for getting localisation. (Setting of lokalisation si failing)

--- test/localisation.js
+++ test/localisation.js
@@ -4,13 +4,32 @@ var request = require('supertest');
 
 
 describe('POST localisation value', function(){
-  it('succussfully sets localisation', function(done){
+  it('successfully sets localisation', function(done){
     request(app)
       .post('/api/v1/localisation/')
       .set('Content-Type', 'application/json')
       .send({key:'type-STAGE-de', value: 'BÜHNE'})
       // .expect({success: 'Localisation added'})
       .expect(200)
+      .end(done);
+  });
+});
+
+describe('GET localised locations', function(){
+  it('successfully localises locations', function(done){
+    request(app)
+      .get('/api/v1/locations?lang=de')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect(function(res) {
+        res.body.forEach(function(location) {
+          if (location.type === 'BÜHNE') {
+            return false;
+          }
+        });
+        return 'Localized string was not found';
+      })
       .end(done);
   });
 });
