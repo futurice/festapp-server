97fd105b8cbf93e2c87b987257bd1a0874da5976
Timo Sand <timo.j.sand@gmail.com>
2014-04-12 14:05:24 +0300

    Added development gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,6 @@
 var jshint = require('gulp-jshint');
 var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
 
 gulp.task('default', ['lint'], function() {
 
@@ -9,4 +10,12 @@ gulp.task('lint', function() {
   gulp.src(['./api/**/*.js', 'server.js', './lib/**/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task('develop', function () {
+  nodemon({ script: 'server.js', ext: 'html js', ignore: ['ignored.js'] })
+    .on('change', ['lint'])
+    .on('restart', function () {
+      console.log('restarted!');
+    });
 });
