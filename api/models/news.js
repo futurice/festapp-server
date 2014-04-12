78fcc312ab6e59566b8708515e7dad8927949c87
Timo Sand <timo.j.sand@gmail.com>
2014-04-12 12:44:23 +0300

    Updated example data

--- api/models/news.js
+++ api/models/news.js
@@ -8,7 +8,7 @@ var newsSchema = new Schema({
   teaser_text: String,
   content: String,
   time: Date,
-  status: String
+  status: Boolean
 });
 
 module.exports = mongoose.model('News', newsSchema);
