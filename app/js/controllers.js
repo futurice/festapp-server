58c342a55fc5d502ae344621f0edffedbb364e9c
Mikko Tamminen <mikko.k.tamminen@gmail.com>
2014-04-12 16:45:26 +0300

    Dynaaminen artistilista

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -11,6 +11,45 @@ myAppControllers.
               $scope.artists = response;
             });
 
+            //Jos tän scheman saa jostain
+            var schema = {
+                name: { type: String, default: '' },
+                picture: String,
+                quote: String,
+                content: String,
+                featured: String,
+                status: String,
+                stage: String,
+                day: String,
+                time_start: Date,
+                time_stop: Date,
+                founded: Number,
+                genre: String,
+                members: [String],
+                albums: [String],
+                highlights: String,
+                youtube: String,
+                spotify: String,
+                contact_info: String,
+                press_image: String,
+                credits: String,
+                place: Number
+            };
+
+            //Voidaan rakentaa taulukko fields
+            $scope.fields = [];
+
+            //Johon tulee oliot joissa on kentät name ja type
+            Object.getOwnPropertyNames(schema).forEach(function(name) {
+                var type;
+                if (schema[name].type)
+                    type = schema[name].type.toString();
+                else
+                    type = schema[name].toString();
+                type = type.split(" ")[1].slice(0,-2);
+                $scope.fields.push({"name": name, "type": type})
+            });
+
             $scope.artist = {
               "id": "1",
               "name": "Bad Finance",
