9ed2b564df88365d07eaf47a473f9f7ad4718a9e
Ville Kukkola <ville.kukkola@helsinki.fi>
2014-04-12 22:52:04 +0300

    Added album search and updated data model.

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var http = require('http');
 var url = require('url');
 var restify = require('express-restify-mongoose');
 var mongoose = require('mongoose');
+var mb = require('musicbrainz');
 
 var lastfm = require('./lib/lastfm')
+
 
 var Artist = require('./api/models/artist');
 var Faq = require('./api/models/faq');
