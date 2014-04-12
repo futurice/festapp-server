cedffae9505582f9df1aca345937edda1655c97a
Timo Sand <timo.j.sand@gmail.com>
2014-04-12 12:07:08 +0300

    api version variable

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.use(logger('short'));
 app.use(bodyParser());
 app.use('/public', express.static(__dirname + '/public'));
 
-
-app.get('/api/v1/localisation/:key', function(req, res) {
+var apiVersion = 'v1';
+app.get('/api/'+apiVersion+'/localisation/:key', function(req, res) {
   var cb = function(res, object) { res.json(object); }.bind(null, res);
   redis.get(req.params.key, function(err, val) {
     if (err) {
@@ -44,14 +44,15 @@ app.get('/api/v1/localisation/:key', function(req, res) {
   });
 });
 
-app.post('/api/v1/localisation', function(req, res) {
+app.post('/api/'+apiVersion+'/localisation', function(req, res) {
   redis.set(req.body.key, req.body.val);
   res.status(200);
   res.json({success: 'Localisation added'});
 });
 
 restify.defaults({
-   outputFn: Localise.localiseApiCallResult
+   outputFn: Localise.localiseApiCallResult,
+   version: apiVersion
 });
 
 restify.serve(app, Artist);
