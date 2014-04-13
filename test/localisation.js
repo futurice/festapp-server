bcf98d233854c7c60a8593020518bfb00d869d3c
Timo Sand <timo.j.sand@gmail.com>
2014-04-13 13:39:22 +0300

    Added data run in travis

--- test/localisation.js
+++ test/localisation.js
@@ -20,6 +20,7 @@ describe('GET localised locations', function(){
     request(app)
       .get('/api/v1/locations?lang=de')
       .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
       .expect(200)
       .expect(function(res) {
         var types = res.body.map(function(location) {
