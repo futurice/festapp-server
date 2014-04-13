f2b4d24c622a2c56661e7f8a7a3e0f5b31727e50
Olli Rissanen <olli.rissanen@helsinki.fi>
2014-04-13 14:36:39 +0300

    Added missing semicolons

--- routes/instagram.js
+++ routes/instagram.js
@@ -1,6 +1,6 @@
 var instagram = require('../lib/instagram');
 
 module.exports = function(app, apiVersion) {
-  app.use('/api' + apiVersion + '/instagram/tag', instagram.tagMedia)
-  app.use('/api' + apiVersion + '/instagram/user', instagram.userMedia
+  app.use('/api' + apiVersion + '/instagram/tag', instagram.tagMedia);
+  app.use('/api' + apiVersion + '/instagram/user', instagram.userMedia);
 }
--- routes/rotten.js
+++ routes/rotten.js
@@ -2,5 +2,5 @@ var rotten = require('../lib/rotten');
 
 module.exports = function(app) {
   app.get('/api/rotten/:query', rotten.rotten);
-  app.get('/api/imdb/:query', imdb.imdb)
+  app.get('/api/imdb/:query', imdb.imdb);
 }
