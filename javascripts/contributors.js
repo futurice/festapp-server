c46dcdb316550afce3edcadfa37309a63dc965bd
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-04-16 09:04:30 +0300

    Add real names to (randomized) contributor lists

--- javascripts/contributors.js
+++ javascripts/contributors.js
@@ -7,64 +7,41 @@
     'festapp-wp',
     'festapp-server'])
     .each(function(project){
-      sendRequest(contributorUrl(project), function (req){
-        document.getElementById(project).innerHTML = processContributorData(req);
+      $.ajax(contributorUrl(project)).then(function (res) {
+        $('#' + project).html(processContributorData(res));
       });
     });
+
+  var users = {};
 
   function contributorUrl(projectName) {
     return 'https://api.github.com/repos/futurice/'+projectName+'/contributors';
   }
 
-  function processContributorData(req){
-    var contributorList = JSON.parse(req.responseText);
-    return _(contributorList).map(templateUser).reduce(listify, "");
+  function processContributorData(contributorList){
+    return _(contributorList)
+      .shuffle() // no-one is more important!
+      .map(function (e) {
+        return $('<li>').append(userLink(e));
+      })
+      .value();
 
-    function listify(output, item) {
-      return output + "<li>" + item + "</li>\n";
-    }
+    function userLink(e) {
+      console.log(e);
+      var element =  $('<a>').attr("href", e.html_url).html(e.login);
 
-    function templateUser(e) {
-      return '<a href="' + e.html_url + '">' + e.login + '</a>';
-    }
-  }
-
-  function createXMLHTTPObject() {
-    var XMLHttpFactories = [
-        function () {return new XMLHttpRequest()},
-        function () {return new ActiveXObject("Msxml2.XMLHTTP")},
-        function () {return new ActiveXObject("Msxml3.XMLHTTP")},
-        function () {return new ActiveXObject("Microsoft.XMLHTTP")}
-    ];
-      var xmlhttp = false;
-      for (var i=0;i<XMLHttpFactories.length;i++) {
-          try {
-              xmlhttp = XMLHttpFactories[i]();
-          }
-          catch (e) {
-              continue;
-          }
-          break;
+      if (!users[e.login]) {
+        users[e.login] = $.ajax(e.url);
       }
-      return xmlhttp;
+
+      users[e.login].then(function (res) {
+        var name = res.name ? res.name + " (" + res.login + ")" : res.login;
+        element.html(name);
+      });
+
+      return element;
+    }
   }
 
-  function sendRequest(url,callback,postData) {
-      var req = createXMLHTTPObject();
-      if (!req) return;
-      var method = (postData) ? "POST" : "GET";
-      req.open(method,url,true);
-      if (postData)
-          req.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-      req.onreadystatechange = function () {
-          if (req.readyState != 4) return;
-          if (req.status != 200 && req.status != 304) {
-              return;
-          }
-          callback(req);
-      };
-      if (req.readyState == 4) return;
-      req.send(postData);
-  }
 
 })(document);
